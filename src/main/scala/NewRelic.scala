2796618da6217e8a6aa792188d000deb6635f034
Sean C. Sullivan <ssullivan@gilt.com>
2017-07-05 10:17:52 -0400

    NewRelic agent 3.40.0

--- src/main/scala/NewRelic.scala
+++ src/main/scala/NewRelic.scala
@@ -51,7 +51,7 @@ object NewRelic extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     ivyConfigurations += nrConfig,
-    newrelicVersion := "3.39.1",
+    newrelicVersion := "3.40.0",
     newrelicAgent := findNewrelicAgent(update.value),
     newrelicAppName := name.value,
     newrelicAttributesEnabled := true,
