ccb1228112cdf46cfdf95d425d75d99ad07f3aee
Sean Sullivan <github@seansullivan.com>
2019-05-29 14:40:15 -0400

    NewRelic 5.1.0

--- src/main/scala/NewRelic.scala
+++ src/main/scala/NewRelic.scala
@@ -56,7 +56,7 @@ object NewRelic extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     ivyConfigurations += NrConfig,
-    newrelicVersion := "4.5.0",
+    newrelicVersion := "5.1.0",
     newrelicAgent := findNewrelicAgent(update.value),
     newrelicAppName := name.value,
     newrelicAttributesEnabled := true,
